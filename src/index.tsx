5f6129141351e9cd72de44b8c676bd004f717c36
Harsh Rastogi <rastogiharsh04@gmail.com>
2021-04-19 11:19:36 +0530

    Boilerplate Setup

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { applyMiddleware, compose, createStore } from "redux";
+import thunk from "redux-thunk";
+import App from "./components/App/App";
+// import history from "./history";
+import reducer from "./Reducers";
+
+console.log(process.env);
+
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
+const Root = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
+
+ReactDOM.render(<Root />, document.getElementById("root"));
